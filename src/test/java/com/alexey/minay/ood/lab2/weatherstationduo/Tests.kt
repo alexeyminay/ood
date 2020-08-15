821b4dc47353c63f97998a30213f64d5c8e6aa4c
alexey.minai <aleksejzuka@gmail.com>
2020-08-16 00:15:16 +0300

    [lab3] добавил тесты

--- src/test/java/com/alexey/minay/ood/lab2/weatherstationduo/Tests.kt
+++ src/test/java/com/alexey/minay/ood/lab2/weatherstationduo/Tests.kt
@@ -1,15 +1,43 @@
 package com.alexey.minay.ood.lab2.weatherstationduo
 
-import com.alexey.minay.ood.lab2.weaterstationduo.Display
-import com.alexey.minay.ood.lab2.weaterstationduo.StatDisplay
+import com.alexey.minay.ood.lab2.weaterstationduo.IObserver
 import com.alexey.minay.ood.lab2.weaterstationduo.WeatherDataIn
 import com.alexey.minay.ood.lab2.weaterstationduo.WeatherDataOut
+import com.alexey.minay.ood.lab2.weaterstationduo.WeatherInfo
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
-class Tests  {
+class Tests {
 
-    val weatherDataOut = WeatherDataOut()
-    val weatherDataIn = WeatherDataIn()
-    val display = Display()
-    val statsDisplay = StatDisplay()
+    private val mWeatherDataOut = WeatherDataOut()
+    private val mWeatherDataIn = WeatherDataIn()
+    private val mDisplay = mock<IObserver<WeatherInfo>>()
+
+    @Before
+    fun register() {
+        mWeatherDataIn.register(1, mDisplay)
+        mWeatherDataOut.register(1, mDisplay)
+    }
+
+    @Test
+    fun shouldGetWeatherDataOutInUpdateMethodParams() {
+        mWeatherDataOut.setMeasurements(1.0, 2.0, 3.0)
+        verify(mDisplay).update(WeatherInfo(1.0, 2.0, 3.0), mWeatherDataOut)
+    }
+
+    @Test
+    fun shouldGetWeatherDataInInUpdateMethodParams() {
+        mWeatherDataIn.setMeasurements(1.0, 2.0, 3.0)
+        verify(mDisplay).update(WeatherInfo(1.0, 2.0, 3.0), mWeatherDataIn)
+    }
+
+    @After
+    fun remove() {
+        mWeatherDataIn.remove(mDisplay)
+        mWeatherDataOut.remove(mDisplay)
+    }
 
 }
